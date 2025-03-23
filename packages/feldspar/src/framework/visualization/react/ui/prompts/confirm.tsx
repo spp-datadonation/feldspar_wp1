cd59891609ca30ae6ad8ddf273e6b5c8d46a731c
frod <frod.fedora.com>
2025-03-23 12:18:46 +0100

    remove cancel option during submission

--- packages/feldspar/src/framework/visualization/react/ui/prompts/confirm.tsx
+++ packages/feldspar/src/framework/visualization/react/ui/prompts/confirm.tsx
@@ -11,14 +11,10 @@ type Props = Weak<PropsUIPromptConfirm> & ReactFactoryContext
 
 export const Confirm = (props: Props): JSX.Element => {
   const { resolve } = props
-  const { text, ok, cancel } = prepareCopy(props)
+  const { text, ok } = prepareCopy(props)
 
   function handleOk (): void {
     resolve?.({ __type__: 'PayloadTrue', value: true })
-  }
-
-  function handleCancel (): void {
-    resolve?.({ __type__: 'PayloadFalse', value: false })
   }
 
   return (
@@ -26,7 +22,6 @@ export const Confirm = (props: Props): JSX.Element => {
       <BodyLarge text={text} margin='mb-4' />
       <div className='flex flex-row gap-4'>
         <PrimaryButton label={ok} onClick={handleOk} color='text-grey1 bg-tertiary' />
-        <PrimaryButton label={cancel} onClick={handleCancel} color='text-white bg-primary' />
       </div>
     </>
   )
@@ -35,13 +30,11 @@ export const Confirm = (props: Props): JSX.Element => {
 interface Copy {
   text: string
   ok: string
-  cancel: string
 }
 
-function prepareCopy ({ text, ok, cancel, locale }: Props): Copy {
+function prepareCopy ({ text, ok, locale }: Props): Copy {
   return {
     text: Translator.translate(text, locale),
     ok: Translator.translate(ok, locale),
-    cancel: Translator.translate(cancel, locale)
   }
 }
